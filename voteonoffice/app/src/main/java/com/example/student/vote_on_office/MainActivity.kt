2e32d001a47a747ce50823ce3242878ef6f440b4
ArtemSnegovskoy <snegovskoy.a@gmail.com>
2018-07-02 15:23:14 +0300

    Revert "Features (#2)" (#5)
    This reverts commit 332d65cbb9a63cddff2ca7a1ee7bae47cd68fad4.

---
+++ voteonoffice/app/src/main/java/com/example/student/vote_on_office/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.student.vote_on_office
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
